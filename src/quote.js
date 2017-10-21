80a3b5ff0e8ca98ca4e064cebd05db19fc3205de
Michael Pacheco <mpacheco@thelotent.com>
2017-10-20 17:38:56 -0700

    generate quote

--- src/quote.js
+++ src/quote.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class QuoteDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://talaikis.com/api/quotes/random/`)
+      .then(res => {
+        const quote = res.data;
+        this.setState({ quote })
+      });
+  }
+
+  reLoad = () => {
+    axios.get(`https://talaikis.com/api/quotes/random/`)
+      .then(res => {
+        const quote = res.data;
+        this.setState({ quote })
+      });  
+  }
+  
   render() {
     return (
       <div>
-        API Request here
+        <h1>{this.state.quote.quote}</h1>
+        <h2>{this.state.quote.author}</h2>
+        <br/>
+        <button onClick={this.reLoad}>New Quote, please!</button>
       </div>
-    )
+    );
   }
-};
+}
 
 export default QuoteDisplay;
 
